017808141eed0b1d403a192769ed44bbbd56e317
Francesco <francesco.moretti2803@gmail.com>
2026-09-23 20:06:52 +0200

    parametri e valori di default creazione pool

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,12 @@
 const mysql=require('mysql2/promise');
 
-function createPool({host, port, user, password, database, ca}){
+function createPool({host, port, user, password, database, ca, waitForConnections=true, connectionLimit=10, queueLimit=0}){
     return mysql.createPool({
         host, port, user, password, database,
         ssl: {ca},
-        waitForConnections: true,
-        connectionLimit: 10,
-        queueLimit: 0
+        waitForConnections,
+        connectionLimit,
+        queueLimit
     });
 };
 
