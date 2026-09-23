dff3df2d4f0cf2a1a8449621a1d9a3ae16c2a148
Francesco <francesco.moretti2803@gmail.com>
2026-09-23 12:25:31 +0200

    funzione di escape (per frontend e backend), codice per slider

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports={
     ...require('./src/utils/validazione'),
     ...require('./src/utils/hash'),
-    ...require('./src/utils/dbKeepAlive')
+    ...require('./src/utils/dbKeepAlive'),
+    ...require('./src/client/utils')
 };
